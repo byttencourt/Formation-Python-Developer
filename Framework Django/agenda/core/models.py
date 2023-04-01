c96f1707d95dbfe9ef40617c28f89620d6216372
byttencourt <byttencourt@hotmail.com>
2023-03-31 23:35:56 -0300

    adicionado sistema de Edição e Exclusão de Agendamento

--- Framework Django/agenda/core/models.py
+++ Framework Django/agenda/core/models.py
@@ -9,6 +9,7 @@ class Evento(models.Model):
     data_evento = models.DateTimeField(verbose_name='Data do evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    local = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
         db_table = 'evento'
@@ -18,4 +19,7 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M hrs.')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
 
