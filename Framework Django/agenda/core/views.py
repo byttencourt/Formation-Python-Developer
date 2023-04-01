536703c4f0be81796f635130d61605d642b90109
byttencourt <byttencourt@hotmail.com>
2023-03-31 21:08:22 -0300

    v1

--- Framework Django/agenda/core/views.py
+++ Framework Django/agenda/core/views.py
@@ -1,13 +1,39 @@
+from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
-# Create your views here.
-
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 def eventos(request, titulo_evento):
     return
 
 
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuário ou senha errada!')
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
